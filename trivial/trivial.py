17e5234b3f0a01ae57ca2263bdd4d8cdc004e720
Jens Eckert <jeckert5@kent.edu>
2023-09-13 17:37:17 -0400

    added if while and begin, started thinking about function then died

--- trivial/trivial.py
+++ trivial/trivial.py
@@ -12,6 +12,36 @@ def set_statement(v, e):
     e[v[1]] = evaluate(v[2], e)
     return None
 
+# includes else statement in v
+def if_statement(v, e):
+    assert len(v) == 4
+    assert type(v[1:]) is list
+    if evaluate(v[1], e):
+        return evaluate(v[2], e)
+    return evaluate(v[3], e)
+
+def while_statement(v, e):
+    assert len(v) == 3
+    assert type(v[1:]) is list
+
+    while evaluate(v[1], env):
+        evaluate(v[2], env)
+    return None
+
+def begin_statement(v, e):
+    assert len(v) == 3
+
+    for item in v[1:]:
+        result = evaluate(item, e)
+    return result
+
+# v[1] == function name, v[2] == arguments, v[3] == return type
+# evaluate function body somehow
+def function(v, e):
+
+    assert len(v) == 4
+    assert type(v[1]) is str and type(v[2]) is list and type(v[3]) is str
+
 def evaluate(v, env):
     if type(v) is list:
         if len(v) == 0:
@@ -19,6 +49,14 @@ def evaluate(v, env):
         assert type(v[0]) is str
         if v[0] == "set":
             return set_statement(v, env)
+        if v[0] == "if":
+            return if_statement(v, env)
+        if v[0] == "while":
+            return while_statement(v, env)
+        if v[0] == "begin":
+            return begin_statement(v, env)
+        # if v[0] == "function":
+        #     return function(v, env)
         #if v[0] == "set":
         #    return set_statement(v, e)
         f = resolve(v[0], env)
@@ -36,7 +74,14 @@ env = {
     '-': lambda v, e: v[0] - v[1],
     '*': lambda v, e: v[0] * v[1],
     '/': lambda v, e: v[0] / v[1],
-    'print': lambda v, e: print(v)
+    'print': lambda v, e: print(v),
+    '<': lambda v, e: v[0] < v[1],
+    '>': lambda v, e: v[0] > v[1],
+    '==': lambda v, e: v[0] == v[1],
+    '<=': lambda v, e: v[0] <= v[1],
+    '>=': lambda v, e: v[0] >= v[1],
+    '!=': lambda v, e: v[0] != v[1],
+    '?': lambda v, e: print(e)
 }
 
 def test_evaluate():
@@ -57,6 +102,17 @@ def test_evaluate():
     assert env['q'] == 11
     evaluate(['print', 2, 3, 4], env)
 
+    assert evaluate(['if', ['<',5,6], ['+', 2, 1], ['-',2,1]], env) == 3
+    assert evaluate(['if', ['>',5,6], ['+', 2, 1], ['-',2,1]], env) == 1
+
+    evaluate(['set', 'i', 0], env)
+
+    evaluate(['?'], env)
+
+    assert evaluate(['while', ['<', 'i', 10], ['set', 'i', ['+', 'i', 1]]], env) == None
+    assert evaluate(['begin', ['-',2,1], ['/', 10,2]], env) == 5
+
+    evaluate(['?'], env)
 
 def test_resolve():
     print("test resolve")
